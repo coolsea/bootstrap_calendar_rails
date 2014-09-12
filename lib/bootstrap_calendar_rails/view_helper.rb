88a8bf6de20a5e19f8b94abbabaa4f9373a75c99
YangTao <bestyt@gmail.com>
2014-09-12 23:16:19 +0800

    fix clas

--- lib/bootstrap_calendar_rails/view_helper.rb
+++ lib/bootstrap_calendar_rails/view_helper.rb
@@ -74,8 +74,8 @@ module BootstrapCalendarRails
         # return header, which contains both desktop and mobile day names
         def header
           content_tag 'div', class: "cal-row-fluid cal-row-head" do
-            build_header(day_names, "cal-span1 visible-md-block") +
-            build_header(mobile_day_names, "cal-span1 hidden-md")
+            build_header(day_names, "cal-span1 visible-lg-block") +
+            build_header(mobile_day_names, "cal-span1 hidden-lg")
           end
         end
 
